d43615baa23530c06267ee9fc2312a8e262a4718
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-07-28 22:38:36 +0700

    style: few adjustments

--- components/Game/GameGrid.vue
+++ components/Game/GameGrid.vue
@@ -7,7 +7,7 @@ const { games } = defineProps(["games"]);
 <template>
   <div class="@container">
     <div
-      class="grid grid-cols-2 gap-2 sm:grid-cols-3 lg:@3xl:grid-cols-4 xl:grid-cols-4 xl:@4xl:grid-cols-5 2xl:grid-cols-5 2xl:@5xl:grid-cols-6"
+      class="grid grid-cols-3 gap-2 lg:@3xl:grid-cols-4 xl:grid-cols-4 xl:@4xl:grid-cols-5 2xl:grid-cols-5 2xl:@5xl:grid-cols-6"
     >
       <NuxtLink v-for="game in games" :to="game.slug">
         <GameCard :game="game" />
