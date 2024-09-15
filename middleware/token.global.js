e5f86a947a5f6802840827ec1be2e80297edcc75
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-09-15 21:29:51 +0700

    change: url to /api/token

--- middleware/token.global.js
+++ middleware/token.global.js
@@ -3,12 +3,9 @@ import { IGDB_ACCESS_TOKEN } from "~/server/utils/constants";
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const config = useRuntimeConfig();
   const access_token = useCookie(IGDB_ACCESS_TOKEN).value;
-  // const isApiRoute = to.path.startsWith("/api");
 
-  // if (!access_token && !isApiRoute) {
   if (!access_token) {
-    const data = await $fetch("https://id.twitch.tv/oauth2/token", {
-      method: "POST",
+    const data = await $fetch("/api/token", {
       params: {
         client_id: config.CLIENT_ID,
         client_secret: config.CLIENT_SECRET,
--- server/api/token.js
+++ server/api/token.js
@@ -3,19 +3,12 @@ import { IGDB_ACCESS_TOKEN } from "../utils/constants";
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const access_token = getCookie(event, IGDB_ACCESS_TOKEN);
+  const { client_id, client_secret, grant_type } = getQuery(event);
 
-  if (!access_token) {
-    const data = await $fetch("https://id.twitch.tv/oauth2/token", {
-      method: "POST",
-      params: {
-        client_id: config.CLIENT_ID,
-        client_secret: config.CLIENT_SECRET,
-        grant_type: "client_credentials",
-      },
-    });
+  const data = await $fetch("https://id.twitch.tv/oauth2/token", {
+    method: "POST",
+    params: { client_id, client_secret, grant_type },
+  });
 
-    setCookie(event, IGDB_ACCESS_TOKEN, data.access_token, {
-      expires: new Date(Date.now() + data.expires_in * 1000), // pastikan multiply by 1000 untuk miliseconds
-    });
-  }
+  return data;
 });
