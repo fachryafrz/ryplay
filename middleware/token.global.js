88246867cda0e6327e3b9124b0f7a7dc5740410d
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-09-17 00:28:34 +0700

    Revert "remove: middleware"
    This reverts commit e58db2c9af90b6c162f4d1d0dd6ad7d7d30aaa71.

---
+++ middleware/token.global.js
@@ -0,0 +1,23 @@
+import { IGDB_ACCESS_TOKEN } from "~/server/utils/constants";
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const config = useRuntimeConfig();
+  const access_token = useCookie(IGDB_ACCESS_TOKEN).value;
+
+  if (!access_token) {
+    const data = await $fetch("https://id.twitch.tv/oauth2/token", {
+      method: "POST",
+      params: {
+        client_id: config.CLIENT_ID,
+        client_secret: config.CLIENT_SECRET,
+        grant_type: "client_credentials",
+      },
+    });
+
+    useCookie(IGDB_ACCESS_TOKEN, {
+      expires: new Date(Date.now() + data.expires_in),
+    }).value = data.access_token;
+
+    navigateTo(from.path);
+  }
+});
