427bf58717d4b297f2b57005295a21fbe896919f
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-01-24 14:41:58 +0700

    style: refine featured games layout and improve responsiveness

--- components/Game/ExpandableCard.vue
+++ components/Game/ExpandableCard.vue
@@ -10,7 +10,12 @@ const lastHoveredIndex = ref(0);
     v-for="(game, index) in games"
     :key="game.slug"
     :to="`/games/${game.slug}`"
-    class="min-w-fit overflow-hidden rounded-xl last:hidden 2xl:last:flex"
+    :class="[
+      'featured-games last:hidden xl:last:flex',
+      {
+        active: hoveredIndex === index || lastHoveredIndex === index,
+      },
+    ]"
     @mouseover="
       hoveredIndex = index;
       lastHoveredIndex = index;
@@ -22,54 +27,34 @@ const lastHoveredIndex = ref(0);
     "
     @blur="hoveredIndex = null"
   >
-    <figure
-      :class="[
-        'featured-games',
-        { active: hoveredIndex === index || lastHoveredIndex === index },
-      ]"
+    <img
+      :src="`https://images.igdb.com/igdb/image/upload/t_720p/${game.cover.image_id}.jpg`"
+      alt=""
+      id="game-cover"
+    />
+
+    <div
+      id="game-screenshot"
+      class="absolute inset-0 opacity-0 transition-opacity duration-700"
     >
       <img
-        :src="`https://images.igdb.com/igdb/image/upload/t_720p/${game.cover.image_id}.jpg`"
-        alt=""
-        id="game-cover"
-        class="block h-full w-full bg-cover bg-center transition-all"
-        draggable="false"
-        loading="lazy"
-        role="presentation"
-        aria-hidden="true"
+        class="tra"
+        :src="`https://images.igdb.com/igdb/image/upload/t_720p/${game.artworks[0].image_id || game.screenshots[0].image_id}.jpg`"
       />
-      <div
-        id="game-screenshot"
-        class="absolute inset-0 opacity-0 transition-opacity duration-700"
-      >
-        <img
-          :src="`https://images.igdb.com/igdb/image/upload/t_720p/${game.artworks[0].image_id || game.screenshots[0].image_id}.jpg`"
-          alt=""
-          id="game-screenshot"
-          class="block h-full w-full bg-cover bg-center transition-all"
-          draggable="false"
-          loading="lazy"
-          role="presentation"
-          aria-hidden="true"
-          @error="
-            $event.target.src = `https://images.igdb.com/igdb/image/upload/t_720p/${game.cover.image_id}.jpg`
-          "
-        />
 
-        <div
-          class="absolute inset-0 z-10 flex flex-col justify-end gap-1 text-pretty p-2 px-6 before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-t before:from-base-100"
+      <div
+        class="absolute inset-0 z-10 flex flex-col justify-end gap-1 text-pretty p-2 px-6 before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-t before:from-base-100"
+      >
+        <h3
+          class="line-clamp-1 text-pretty text-center text-sm font-semibold lg:text-start lg:text-xl"
         >
-          <h3
-            class="line-clamp-1 text-pretty text-center text-sm font-semibold lg:text-start lg:text-xl"
-          >
-            {{ game.name }}
-          </h3>
-          <span
-            class="before-content line-clamp-2 text-pretty text-sm text-neutral-300 xl:max-w-[80%]"
-            :data-before-content="game.storyline || game.summary"
-          />
-        </div>
+          {{ game.name }}
+        </h3>
+        <span
+          class="before-content line-clamp-2 text-pretty text-sm text-neutral-300 xl:max-w-[80%]"
+          :data-before-content="game.storyline || game.summary"
+        />
       </div>
-    </figure>
+    </div>
   </NuxtLink>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -95,9 +95,7 @@ const wantToPlay = computed(() => multiquery.value?.wantToPlay);
       <div class="flex flex-col gap-2">
         <h2 class="heading-2">Upcoming Games</h2>
 
-        <div
-          class="grid grid-cols-2 gap-2 md:grid-cols-4 2xl:flex 2xl:overflow-x-auto"
-        >
+        <div class="grid grid-cols-2 lg:flex gap-2 overflow-auto">
           <GameExpandableCard :games="upcoming" />
         </div>
       </div>
