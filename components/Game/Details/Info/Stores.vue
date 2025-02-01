4751aff499c2c4b1c41c49f17077ea7c2ce371e3
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-01 17:17:32 +0700

    feat: open external game store in a popup window

--- components/Game/Details/Info/Stores.vue
+++ components/Game/Details/Info/Stores.vue
@@ -3,16 +3,26 @@ const { externalGames, findStoreById } = defineProps([
   "externalGames",
   "findStoreById",
 ]);
+
+const handleOpenWindow = async (url) => {
+  // NOTE: Already tried the documentPictureInPicture with iframe but it doesn't work, because most of the website refuse to be embeded in iframe
+
+  const windowFeatures = `left=${screen.width},top=${screen.height},width=${1200},height=${600},noreferrer,noopener`;
+
+  window.open(url, "gameStoreWindow", windowFeatures);
+
+  return;
+};
 </script>
 
 <template>
   <ul class="flex flex-wrap gap-2">
     <li v-for="externalGame in externalGames" :key="externalGame.category">
       <div
-        className="tooltip tooltip-bottom"
+        className="tooltip tooltip-secondary tooltip-bottom"
         :data-tip="findStoreById(externalGame.category).store"
       >
-        <NuxtLink :to="externalGame?.url" target="_blank">
+        <button @click="handleOpenWindow(externalGame.url)">
           <img
             :src="`/store_icons/${findStoreById(externalGame.category).image_id}.png`"
             :alt="''"
@@ -22,7 +32,7 @@ const { externalGames, findStoreById } = defineProps([
             role="presentation"
             aria-hidden="true"
           />
-        </NuxtLink>
+        </button>
       </div>
     </li>
   </ul>
