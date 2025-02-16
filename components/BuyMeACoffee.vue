07dd98caef305011b490da3b65590e3032bdae0f
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-02-16 15:30:13 +0700

    feat: add BuyMeACoffee component and enhance header layout for better responsiveness

---
+++ components/BuyMeACoffee.vue
@@ -0,0 +1,29 @@
+<script setup>
+const handleOpenWindow = async (url) => {
+  // NOTE: Already tried the documentPictureInPicture with iframe but it doesn't work, because most of the website refuse to be embeded in iframe
+
+  const width =
+    screen.availWidth < 1024 ? 600 : screen.availWidth < 1280 ? 1024 : 1200;
+  const height = screen.availHeight < 600 ? screen.availHeight : 600;
+  const left = (screen.availWidth - width) / 2;
+  const top = (screen.availHeight - height) / 2;
+
+  const windowFeatures = `left=${left},top=${top},width=${width},height=${height},noreferrer,noopener`;
+
+  window.open(url, "gameStoreWindow", windowFeatures);
+};
+</script>
+
+<template>
+  <button
+    @click="handleOpenWindow('https://www.buymeacoffee.com/fachryafrz')"
+    target="_blank"
+    class="btn px-0 overflow-hidden"
+  >
+    <img
+      src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+      alt="Buy Me A Coffee"
+      draggable="false"
+    />
+  </button>
+</template>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -55,6 +55,7 @@ const signOut = async () => {
           title="Already Played"
           empty-message="Haven't played yet."
           table="already_played"
+          class="md:col-span-2 lg:col-span-1"
         />
       </ClientOnly>
     </section>
