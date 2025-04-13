2b6001133125e82b0b18389269dad299c454d2fe
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-04-13 13:14:59 +0700

    chore: update media component to enhance play button visibility and adjust opacity

--- components/Game/Details/Media.vue
+++ components/Game/Details/Media.vue
@@ -157,7 +157,7 @@ onMounted(() => {
               alt=""
               role="presentation"
               aria-hidden="true"
-              class="block h-full w-full"
+              class="block h-full w-full opacity-50"
               draggable="false"
               loading="lazy"
             />
@@ -168,24 +168,11 @@ onMounted(() => {
               <span
                 class="flex aspect-[4/3] justify-center rounded text-center text-base-100"
               >
-                <svg
-                  height="24px"
-                  style="enable-background: new 0 0 32 32"
-                  version="1.1"
-                  viewBox="0 0 32 32"
-                  width="24px"
-                  xml:space="preserve"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                >
-                  <g id="Layer_1" />
-                  <g id="play_x5F_alt">
-                    <path
-                      d="M16,0C7.164,0,0,7.164,0,16s7.164,16,16,16s16-7.164,16-16S24.836,0,16,0z M10,24V8l16.008,8L10,24z"
-                      style="fill: #ffffff"
-                    />
-                  </g>
-                </svg>
+                <Icon
+                  name="ion:play-circle-outline"
+                  size="40"
+                  class="text-white"
+                />
               </span>
             </div>
           </figure>
