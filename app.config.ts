6143a719be3ee28822bd79e624b2d3ac3bd95ad7
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-26 10:36:51 +0700

    chore: update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   name: "Ryplay",
   description:
-    "Ryplay is a comprehensive gaming platform designed to enhance your gaming experience. You can browse, search games with advanced filters and check where a game is available to get/purchase across various game stores. You can also save your favorite games, create a wishlist, and mark games as played.",
+    "Gaming platform designed to enhance your gaming experience. You can browse, search games with advanced filters and check where a game is available to get/purchase across various game stores. You can also save your favorite games, create a wishlist, and mark games as played.",
   url:
     process.env.NODE_ENV === "production"
       ? "https://ryplay.fachryafrz.com"
