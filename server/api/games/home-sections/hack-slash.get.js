f2aba3f21f4ab753314dfe9904cf1ed379da5c65
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2026-03-11 10:59:36 +0700

    refactor(fetch): switch to client side fetching

---
+++ server/api/games/home-sections/hack-slash.get.js
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const access_token = await getAccessToken();
+
+  const fetchData = async (access_token) => {
+    const data = await $fetch(`${config.API_URL}/games`, {
+      method: "POST",
+      headers: {
+        "Client-ID": config.CLIENT_ID,
+        Authorization: `Bearer ${access_token}`,
+      },
+      body: `
+        f *, cover.*, artworks.*, screenshots.*;
+        w cover != null & genres.slug = "hack-and-slash-beat-em-up" & screenshots != null & artworks != null & game_type = 0;
+        s hypes desc;
+        l 20;
+      `,
+    });
+
+    return data;
+  };
+
+  try {
+    return await fetchData(access_token);
+  } catch (error) {
+    return Response.json(
+      { error: error?.response || error },
+      { status: error?.response?.status || 500 },
+    );
+  }
+});
